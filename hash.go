ba934d94a7e01ab86de8d086b5b470ea823b5f80
BRUHItsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2024-02-21 12:13:10 -0600

    add more utilities

--- hash.go
+++ hash.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
+	"hash"
 )
 
 func MD5hash(args ...[]byte) []byte {
@@ -24,6 +25,14 @@ func SHA256hash(args ...[]byte) []byte {
 
 func SHA1hash(args ...[]byte) []byte {
 	digester := sha1.New()
+	for _, msgBytes := range args {
+		digester.Write(msgBytes)
+	}
+	return digester.Sum(nil)
+}
+
+func InterfaceHash(digester hash.Hash, args ...[]byte) []byte {
+	digester.Reset()
 	for _, msgBytes := range args {
 		digester.Write(msgBytes)
 	}
--- hmac.go
+++ hmac.go
@@ -2,10 +2,20 @@ package crypto_utils
 
 import (
 	"crypto/hmac"
+	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
 	"github.com/OneOfOne/xxhash"
+	"hash"
 )
+
+func HMACofMD5(key []byte, args ...[]byte) []byte {
+	digester := hmac.New(md5.New, key)
+	for _, msgBytes := range args {
+		digester.Write(msgBytes)
+	}
+	return digester.Sum(nil)
+}
 
 func HMACofSHA1(key []byte, args ...[]byte) []byte {
 	digester := hmac.New(sha1.New, key)
@@ -25,6 +35,17 @@ func HMACofSHA256(key []byte, args ...[]byte) []byte {
 
 func HMACofXXHash(key []byte, args ...[]byte) []byte {
 	digester := hmac.New(xxhash.NewHash32, key) //Using 64bit or 32bit?
+	for _, msgBytes := range args {
+		digester.Write(msgBytes)
+	}
+	return digester.Sum(nil)
+}
+
+func HMACofInterfaceHash(algo hash.Hash, key []byte, args ...[]byte) []byte {
+	algo.Reset()
+	digester := hmac.New(func() hash.Hash {
+		return algo
+	}, key)
 	for _, msgBytes := range args {
 		digester.Write(msgBytes)
 	}
